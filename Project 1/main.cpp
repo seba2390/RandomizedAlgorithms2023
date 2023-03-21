a0d40ab63f0100641d52e9897b1890892a7c2e3c
seba2390 <madsen3008@gmail.com>
2023-03-21 15:28:10 +0100

    updated ticks on plots

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -14,15 +14,15 @@ static_assert((CHAR_BIT * sizeof(key_type) == KEY_BIT_SIZE), "Adjust key_type to
 
 int main()
 {
-    const unsigned int iterations = 18;
-    const unsigned int seed_multiplier = 11;
+    const unsigned int iterations = 14;
+    const unsigned int seed_multiplier = 7;
 
     //// ----------------- Testing Hashing With Chaining implementation ----------------- ////
     std::cout << " \n-------- Hashing with Chaining --------\n " << std::endl;
 
     using hash_table = HashingWithChaining<key_type, array_type , linked_list_type>;
 
-    unsigned int nr_seeds = 50;
+    unsigned int nr_seeds = 500;
     std::string folder_path = "../../Data/HashingWithChaining";
     for(unsigned int seed = 0; seed < nr_seeds; seed++)
     {
@@ -71,7 +71,7 @@ int main()
     std::cout << " \n-------- Red-Black Tree --------\n " << std::endl;
     using red_black_tree = RedBlackTree<key_type, array_type>;
 
-    nr_seeds = 50;
+    nr_seeds = 500;
     folder_path = "../../Data/RedBlackTree";
     for(unsigned int seed = 0; seed < nr_seeds; seed++)
     {
@@ -115,7 +115,7 @@ int main()
     std::cout << " \n-------- Perfect Hashing --------\n " << std::endl;
 
     using PerfectHashing = PerfectHashing<key_type, array_type, linked_list_type>;
-    nr_seeds = 50;
+    nr_seeds = 500;
     folder_path = "../../Data/PerfectHashing";
     for(unsigned int seed = 0; seed < nr_seeds; seed++)
     {
